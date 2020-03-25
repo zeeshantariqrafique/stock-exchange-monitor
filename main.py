78ae983e596fd548dc398f27d96666babe48abce
ZeeshanTariqRafique <zeeshantariqrafique@gmail.com>
2020-03-25 12:02:37 +0530

    nse_api 1 : Added whatsapp functionality + gitignore

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import nsetools
+import whatsapp
 from nsetools import Nse
 import mail
 nse = Nse()
 d = nse.get_index_quote("nifty 50")
-sender_list =["test@gmail.com","test@gmail.com"]
-for key in d:
+sender_whatsapp_list = ["+91-9999999999"] # some dummy number
+sender_list =["test@gmail.com","test@gmail.com"] #some dummy email 
+for key , value in d.items():
     if key == "pChange":
-        if(float(d[key]) > 2):
-            mail.sendMailTo(sender_list,d[key])
+        if(float(value) > 2):
+            mail.sendMailTo(sender_list,value)
+            whatsapp.sendWhatsApp(sender_whatsapp_list,value)
---
+++ whatsapp.py
@@ -0,0 +1,15 @@
+import os
+from datetime import datetime
+from twilio.rest import Client
+
+
+from_whatsapp_number='whatsapp:+14155238886'
+
+def sendWhatsApp(list_numbers,nifty_change):
+    msg = '''Hi, 
+        This message is to inform you that Nifty 50 Index has changed by ''' + str(nifty_change) + '%' + 'as of' + str(datetime.now())
+    for number in list_numbers:  
+        to_num ='whatsapp:' + number
+        client= Client()
+        client.messages.create(body=msg,from_=from_whatsapp_number,to=to_num)
+        print('Sending whatsapp to '+ number)
