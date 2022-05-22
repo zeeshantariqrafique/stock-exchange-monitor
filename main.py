0135e9e86f6f6f38e51c55e07ec0e6d5b39210e3
zeeshantariqrafique <zeeshantariqrafique@gmail.com>
2022-05-22 11:37:40 +0530

    Add requirements.txt + read all config dynamically

--- main.py
+++ main.py
@@ -11,19 +11,13 @@ if __name__ == "__main__":
     #Load the config file , See nse.yml
     with open(r'nse.yml') as file:
         data = yaml.full_load(file)
-
-    for k , v in data["whatsapp"].items():
-        if k == "contact_path" and v is not None:
-            #contact_path : Holds fully qualified path of excel file . This file has a column 'Phone' that has 10 digit mobile numbers
-            contact_path = v
-
-    for k , v in data["email"].items(): 
-        if k == "sender_email" and v is not None:
-            sender_email = v
-        if k == "sender_password" and v is not None:
-            sender_password = v
-
-    df = pd.read_excel(contact_path)
+    
+    config = {}
+    
+    for k , v in {**data["whatsapp"],**data["email"]}.items():
+        config[k] = v
+        
+    df = pd.read_excel(config['contact_path'])
     df['Phone'] = df['Phone'].apply(lambda x: '+91' + str(x)) #Hardcoding +91 ext on India , Should be enhanced later
     nse = Nse()
 
@@ -36,9 +30,9 @@ if __name__ == "__main__":
                 #Check if the percentage change is negative or positive , If negative send whatsapp
                 if float(value) < 0:
                     whatsapp.sendWhatsApp(df['Phone'],value)
-                   # print(df['Email'])
-                 #   print(df['Email'].tolist())
-                    mail.sendMailTo(sender_email,sender_password,df['Email'].dropna().tolist(),value)
+                    #print(df['Email'])
+                    #print(df['Email'].tolist())
+                    mail.sendMailTo(config['sender_email'],config['sender_password'],df['Email'].dropna().tolist(),value)
                 else:
                     print('Not sending notification as change is postive  : '+str(value))
         print('Going to sleep for 30 mins at '+ str(datetime.now()))
